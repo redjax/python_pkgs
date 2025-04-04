322d65dad3e449c799472f26321716ef4c1c4a64
redjax <no@none.com>
2025-04-04 11:51:42 -0400

    Remove old Dynaconf settings objects.
    Rely on the .get_namespace() method or manually defining Dynaconf settings objects in code.

--- libs/settings-lib/src/settings/__init__.py
+++ libs/settings-lib/src/settings/__init__.py
@@ -1,8 +1,3 @@
 from __future__ import annotations
 
-from .api_settings import FASTAPI_SETTINGS, UVICORN_SETTINGS
-from .app_settings import APP_SETTINGS
-from .celery_settings import CELERY_SETTINGS
-from .db_settings import DB_SETTINGS
-from .github_settings import GITHUB_SETTINGS
-from .logging_settings import LOGGING_SETTINGS
+from .base import *
--- libs/settings-lib/src/settings/github_settings.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from dynaconf import Dynaconf
-
-GITHUB_SETTINGS = Dynaconf(
-    environments=True,
-    envvar_prefix="GH",
-    settings_files=["settings.toml", ".secrets.toml"],
-)
