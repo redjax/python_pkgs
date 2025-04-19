30566acfbc3d6ff2d328129aceb763b946eac880
redjax <no@none.com>
2025-04-19 17:34:56 -0400

    Add old script version to platform_lib.script

---
+++ sandbox/platform_lib/script_version.py
@@ -0,0 +1,8 @@
+from platform_lib import script as platform_script
+
+if __name__ == "__main__":
+    args = platform_script.get_args()
+
+    platform_script._set_logging_level(verbosity=args.verbosity, set_debug=args.debug)
+
+    platform_script.main(options=args)
