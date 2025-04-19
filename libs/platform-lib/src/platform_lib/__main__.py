d0fc7958fc4fa30b6ecea00b0c8f2cbbd965412f
redjax <no@none.com>
2025-04-19 17:39:36 -0400

    Add __main__.py to platform-lib

---
+++ libs/platform-lib/src/platform_lib/__main__.py
@@ -0,0 +1,13 @@
+import logging
+
+from platform_lib.helpers import _set_logging_level, parse_args
+from platform_lib.main import main
+
+log = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    _set_logging_level(verbosity=args.verbosity, set_debug=args.debug)
+
+    main(options=args)
