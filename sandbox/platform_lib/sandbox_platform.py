8526c20773a9c452e8552bc863b64bf86feb8abd
redjax <no@none.com>
2025-04-19 16:27:48 -0400

    Add platform-lib.
    Copy Python platform script into package. TODO: modularize

---
+++ sandbox/platform_lib/sandbox_platform.py
@@ -0,0 +1,9 @@
+import logging
+
+import platform_lib
+
+log = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    platform_info: platform_lib.PlatformInfo = platform_lib.get_platform_info()
+    platform_info.display_info()
